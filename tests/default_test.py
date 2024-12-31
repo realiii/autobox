56700f960907af26c4241d3bfd31d0fbb182af62
Jason Humber <jlhumber@openiii.com>
2024-12-31 15:51:30 -0600

    #52 Add parameterized tests for CellSizeXY exception and repr
    This commit introduces two parameterized tests for the CellSizeXY class. The first verifies exceptions for invalid inputs, while the second checks the string representation method for various valid inputs. This ensures better coverage and robustness of the CellSizeXY implementation.

--- tests/default_test.py
+++ tests/default_test.py
@@ -4,9 +4,36 @@ Test Default Value Classes
 """
 
 
-from pytest import raises
+from pytest import mark, raises
 
-from autobox.default import CellSizeXY
+from autobox.default import (
+    BaseRangeDomain, CellSizeXY, XDomain, XYDomain,
+    YDomain)
+
+
+@mark.parametrize('x, y, exception', [
+    (None, None, TypeError),
+    (-1, -2, ValueError),
+])
+def test_cell_size_xy_raises(x, y, exception):
+    """
+    Test Cell Size XY exceptions
+    """
+    with raises(exception):
+        CellSizeXY(x=x, y=y)
+# End test_cell_size_xy_raises function
+
+
+@mark.parametrize('x, y, expected', [
+    (1, 2, '1 2'),
+    (1.11, 2.22, '1.11 2.22'),
+])
+def test_cell_size_xy_repr(x, y, expected):
+    """
+    Test Cell Size XY repr
+    """
+    assert repr(CellSizeXY(x=x, y=y)) == expected
+# End test_cell_size_xy_repr function
 
 
 def test_cell_size_xy():
